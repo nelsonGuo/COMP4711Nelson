22d0cf76f987d11899ff2481cb3c7d5d7b08a3ab
nelson <nelson@DESKTOP-AJIB52R.hitronhub.home>
2017-09-08 15:48:26 -0700

    setup

---
+++ Student.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Student
+ *
+ * @author nelson
+ */
+class Student {
+    function _construct(){
+        $this->surname = '';
+        $this->first_name ='';
+        $this->emails = array();
+        $this->grades = array();
+        
+    }
+}
